3f820e33599e0cc52833adcaa09805c06f7ccc22
Amy <leiamy12@gmail.com>
2021-01-23 15:57:35 -0500

    Integrate music form into gallery

--- app.py
+++ app.py
@@ -26,14 +26,16 @@ def add_show():
     elif request.method == 'GET':
         return jsonify({'all_shows': []}), 200
 
-@app.route('/add-music', methods=['POST', 'GET'])
+@app.route('/api/music', methods=['POST', 'GET'])
 def add_music():
     if request.method == 'POST':
-        u = request.form['username']
-        l = request.form['link']
-        print(insert_music(u,l))
-        return redirect(url_for('index'))
-    return render_template('add_music.html')
+        body = request.json
+        username = body.get('username', '')
+        link = body.get('link', '')
+        print(insert_music(username, link))
+        return jsonify(body), 201
+    elif request.method == 'GET':
+        return jsonify({'all_music': []}), 200
 
 @app.route('/clear-shows')
 def clear_shows():
--- static/index.js
+++ static/index.js
@@ -9,34 +9,62 @@ window.addEventListener('load', () => {
     };
 }, false);
 
-// Open specific model when user clicks on button
+// Toggle open/close form
 const openForm = () => {
     document.getElementById(`add-container`).style.display = 'flex';
 }
 
-// Close modal
 const closeForm = () => {
     document.getElementById(`add-container`).style.display = 'none';
 }
 
-// Hide username field if user wishes to be anonymous
+// Toggle username field
 const hideUsername = () => {
     document.getElementById('user').style.display = 'none';
 }
-
-// Show username input field
 const showUsername = () => {
     document.getElementById('user').style.display = 'block';
 }
 
-const addShow = async () => {
-    // Get body values
-    const username = document.getElementById('show-username').value;
-    const title = document.getElementById('show-title').value;
-    const link = document.getElementById('show-link').value;
+// Toggle between which form content to show
+const showFormBody = (type) => {
+    const showForm = document.getElementById('body-shows');
+    const musicForm = document.getElementById('body-music');
+
+    switch (type) {
+        case 'shows':
+            showForm.style.display = 'block';
+            musicForm.style.display = 'none';
+            break;
+        case 'music':
+            showForm.style.display = 'none';
+            musicForm.style.display = 'block';
+            break;
+        default:
+            break;
+    }
+}
+
+const addPost = async () => {
+    // Determine the type post being added 
+    const type = document.getElementById('shows-radio').checked ? 'shows' : 'music';
+    const username = document.getElementById('username-input').value;
+
+    if (type === 'shows') {
+        await addShow(username);
+    } else if (type === 'music') {
+        await addMusic(username);
+    };
+
+    closeForm();
+}
+
+const addShow = async (username) => {
+    const title = document.getElementById('title-input').value;
+    const link = document.getElementById('show-link-input').value;
+    const body = { username, title, link };
     
     // Send request
-    const body = { username, title, link }; 
     try {
         const res = await fetch('/api/shows', {
             method: 'POST',
@@ -46,7 +74,22 @@ const addShow = async () => {
         // TODO: Add it in as a new item
     } catch (err) {
         console.log(err);
-    } finally {
-        closeForm('shows');
     }
-}
+};
+
+const addMusic = async (username) => {
+    const link = document.getElementById('music-link-input').value;
+    const body = { username, link };
+    
+    // Send request
+    try {
+        const res = await fetch('/api/music', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json', },
+            body: JSON.stringify(body),
+        });
+        // TODO: Add it in as a new item
+    } catch (err) {
+        console.log(err);
+    }
+};
