0a67d2f589be939881f19f19e407e7147a1cadf3
hanna-yang <hanna.yang.625@gmail.com>
2021-01-23 11:45:12 -0500

    form connected to db

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from db import *
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,7 +9,9 @@ def index():
 @app.route('/add-show', methods=['POST', 'GET'])
 def add_show():
     if request.method == 'POST':
-        print(request.form['username'])
-        print(request.form['title'])
-        print(request.form['link'])
+        u = request.form['username']
+        t = request.form['title']
+        l = request.form['link']
+        print(insert_show(u,t,l))
+        return redirect(url_for('index'))
     return render_template('add_show.html')
--- db.py
+++ db.py
@@ -1,1 +1,12 @@
-import pymongo
+from pymongo import MongoClient
+from secrets_ import MONGO_URI
+
+def insert_show(username, title, link, client=MongoClient(MONGO_URI)):
+    show = client.gallery.show
+    show_info = {
+        'username' : username,
+        'title' : title,
+        'link' : link
+    }
+    show.insert_one(show_info)
+    return 'successfully inserted ' + str(show_info) + ' into db'
