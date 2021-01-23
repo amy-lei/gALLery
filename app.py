4c8288b603acb028a4bac2012a30dfe1c801112a
hanna-yang <hanna.yang.625@gmail.com>
2021-01-22 23:58:20 -0500

    make form template

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello World!'
+
+@app.route('/add-show')
+def add_show():
+    return render_template('add_show.html')
