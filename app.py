03ce9b71a66e5a6348606115d451e8b21f966ffe
Amy <leiamy12@gmail.com>
2021-01-23 15:14:28 -0500

    Integrate form with gallery

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify, make_response
 from db import *
 
 app = Flask(__name__)
@@ -14,15 +14,17 @@ def index():
     }
     return render_template('gallery.html', posts=DATA)
 
-@app.route('/add-show', methods=['POST', 'GET'])
+@app.route('/api/shows', methods=['POST', 'GET'])
 def add_show():
     if request.method == 'POST':
-        u = request.form['username']
-        t = request.form['title']
-        l = request.form['link']
-        print(insert_show(u,t,l))
-        return redirect(url_for('index'))
-    return render_template('add_show.html')
+        body = request.json
+        username = body.get('username', '')
+        title = body.get('title', '')
+        link = body.get('link', '')
+        print(insert_show(username, title, link))
+        return jsonify(body), 201
+    elif request.method == 'GET':
+        return jsonify({'all_shows': []}), 200
 
 @app.route('/add-music', methods=['POST', 'GET'])
 def add_music():
--- static/index.js
+++ static/index.js
@@ -9,14 +9,44 @@ window.addEventListener('load', () => {
     };
 }, false);
 
-const test = (type) => {
-    document.getElementById(`add-${type}-container`).style.opacity = 1;
-    document.getElementById(`add-${type}-container`).style.visibility = "visible";
+// Open specific model when user clicks on button
+const openForm = () => {
+    document.getElementById(`add-container`).style.display = 'flex';
 }
 
-const closeForm = (type) => {
-    document.getElementById(`add-${type}-container`).style.opacity = 0;
-    setTimeout(() => {
-        document.getElementById(`add-${type}-container`).style.visibility = "hidden";
-    }, 3000);
+// Close modal
+const closeForm = () => {
+    document.getElementById(`add-container`).style.display = 'none';
+}
+
+// Hide username field if user wishes to be anonymous
+const hideUsername = () => {
+    document.getElementById('user').style.display = 'none';
+}
+
+// Show username input field
+const showUsername = () => {
+    document.getElementById('user').style.display = 'block';
+}
+
+const addShow = async () => {
+    // Get body values
+    const username = document.getElementById('show-username').value;
+    const title = document.getElementById('show-title').value;
+    const link = document.getElementById('show-link').value;
+    
+    // Send request
+    const body = { username, title, link }; 
+    try {
+        const res = await fetch('/api/shows', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json', },
+            body: JSON.stringify(body),
+        });
+        // TODO: Add it in as a new item
+    } catch (err) {
+        console.log(err);
+    } finally {
+        closeForm('shows');
+    }
 }
