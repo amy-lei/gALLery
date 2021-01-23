3df635482489e29fe60585b34ed904b59eeac3e7
hanna-yang <hanna.yang.625@gmail.com>
2021-01-23 00:07:00 -0500

    add form template, still need to connect it to db

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello World!'
 
-@app.route('/add-show')
+@app.route('/add-show', methods=['POST', 'GET'])
 def add_show():
+    if request.method == 'POST':
+        print(request.form['username'])
+        print(request.form['title'])
+        print(request.form['link'])
     return render_template('add_show.html')
